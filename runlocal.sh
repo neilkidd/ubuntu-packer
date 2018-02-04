5d090176e97819414881ec79b4fbe4b971601a9a
neilkidd <neil.a.kidd@gmail.com>
2018-02-04 13:10:43 +0000

    comment the script as it's a 1 liner!

--- runlocal.sh
+++ runlocal.sh
@@ -2,6 +2,6 @@
 set -x
 
 # Local dev convenience script
-# uses jq to remove push and post-processors
-# Displays the virtualbox UI for easier debugging
+# uses jq to remove push and last post-processor
+# Displays the virtualbox UI for easier debugging See: https://www.packer.io/docs/templates/user-variables.html
 jq 'del(.push) | del(.["post-processors"][][1:])' template.json | packer build -var 'headless=false' -only=virtualbox-iso -
